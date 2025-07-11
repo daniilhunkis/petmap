9723c0183b46cf84adb3140fe5943390ed641772
daniilhunkis <yshkodaniil19@gmail.com>
2025-07-11 17:14:56 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./Root";
 //import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
